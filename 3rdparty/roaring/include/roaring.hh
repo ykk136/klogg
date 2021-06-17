6722bd07d04cf72487b51f2db20a48b44ed09c8f
Anton Filimonov <anton.filimonov@gmail.com>
2021-06-17 20:19:00 +0300

    chore: update CRoaring fix for 32 bit

--- 3rdparty/roaring/include/roaring.hh
+++ 3rdparty/roaring/include/roaring.hh
@@ -19,6 +19,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <cstdint>
 #define ROARING_API_NOT_IN_GLOBAL_NAMESPACE  // see remarks in roaring.h
 #include "roaring.h"
 #undef ROARING_API_NOT_IN_GLOBAL_NAMESPACE
@@ -1119,9 +1120,7 @@ class Roaring64Map {
         //
         // we put std::numeric_limits<>::max/min in parenthesis
         // to avoid a clash with the Windows.h header under Windows
-        return (roarings.size() > 0 && (
-            roarings.size() == roarings.max_size() ||
-            roarings.size() == ((size_t)(std::numeric_limits<uint32_t>::max)()) + 1))
+        return roarings.size() == ((uint64_t)(std::numeric_limits<uint32_t>::max)()) + 1
                    ? std::all_of(
                          roarings.cbegin(), roarings.cend(),
                          [](const std::pair<uint32_t, Roaring> &roaring_map_entry) {
