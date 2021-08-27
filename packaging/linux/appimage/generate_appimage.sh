63a26634d3bc69d0c56130c940580ea036e81358
Anton Filimonov <anton.filimonov@gmail.com>
2021-08-28 00:12:51 +0300

    ci: try building in containers for linux

---
+++ packaging/linux/appimage/generate_appimage.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+DESTDIR=$(readlink -f appdir) ninja install
+wget -c -q "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
+chmod a+x linuxdeployqt-continuous-x86_64.AppImage
+VERSION=$KLOGG_VERSION ./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -bundle-non-qt-libs
+VERSION=$KLOGG_VERSION ./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -appimage
+cp ./klogg-$KLOGG_VERSION-x86_64.AppImage ./packages/klogg-$KLOGG_VERSION-x86_64.AppImage
