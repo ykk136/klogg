ae43ffb814bc6e944dd0da2a79d565fa147e84d7
Anton Filimonov <anton.filimonov@gmail.com>
2022-02-20 21:10:56 +0100

    fix: fix tbb malloc linking

--- 3rdparty/onetbb/include/oneapi/tbb/tbbmalloc_proxy.h
+++ 3rdparty/onetbb/include/oneapi/tbb/tbbmalloc_proxy.h
@@ -38,9 +38,9 @@ tbbmalloc_proxy.lib /INCLUDE:"__TBB_malloc_proxy"
 #if _MSC_VER
 
 #ifdef _DEBUG
-    #pragma comment(lib, "tbbmalloc_proxy_debug.lib")
+    #pragma comment(lib, "klogg_tbbmalloc_proxy_debug.lib")
 #else
-    #pragma comment(lib, "tbbmalloc_proxy.lib")
+    #pragma comment(lib, "klogg_tbbmalloc_proxy.lib")
 #endif
 
 #if defined(_WIN64)
