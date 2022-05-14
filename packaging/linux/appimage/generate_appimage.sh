f5dc701f59127b7a240458adfc49ec3af205b108
Anton Filimonov <anton.filimonov@gmail.com>
2022-05-15 00:14:14 +0200

    ci: fix appimage packaging

--- packaging/linux/appimage/generate_appimage.sh
+++ packaging/linux/appimage/generate_appimage.sh
@@ -5,4 +5,5 @@ wget -c -q "https://github.com/probonopd/linuxdeployqt/releases/download/continu
 chmod a+x linuxdeployqt-continuous-x86_64.AppImage
 VERSION=$KLOGG_VERSION ./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -bundle-non-qt-libs
 VERSION=$KLOGG_VERSION ./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -appimage
+mkdir ./packages
 cp ./klogg-$KLOGG_VERSION-x86_64.AppImage ./packages/klogg-$KLOGG_VERSION-x86_64.AppImage
